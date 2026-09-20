9f12f8c271e64a5257ba842e15dbcbb5b5a0bb37
adityasriram05 <aditya.sriram54008@gmail.com>
2026-09-20 22:41:52 +0530

    url endpoints for yearly and all_data

--- main.py
+++ main.py
@@ -19,5 +19,20 @@ def about(station, date):
             "date":date,
             "temperature": temperature}
 
+@app.route("/api/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID"+str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename, skiprows = 20, parse_dates=['    DATE'])
+    result = df.to_dict(orient = "records")
+    return result
+
+@app.route("/api/yearly/<station>/<year>")
+def all_year(station, year):
+    filename = "data_small/TG_STAID"+str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename, skiprows = 20)
+    df['    DATE'] = df['    DATE'].astype(str)
+    result = df[df['    DATE'].str.startswith(str(year))].to_dict(orient = "records")
+    return result
+
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True,port = 5002)
